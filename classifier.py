1acbd457380abedecab557c3cf2efe0ea1f9b7f6
aakashprasad <52214100+aakashprasad@users.noreply.github.com>
2022-07-02 12:41:16 +0530

    Additon of Polynomial Decison Boundary

--- classifier.py
+++ classifier.py
@@ -5,7 +5,7 @@ import csv
 
 j=0
 dataMatrix=np.ones((100,3))
-feature_set=np.ones((100,2))
+feature_set=np.ones((100,3))
 labels=np.ones((100,1))
 file=open("data.csv")
 data=csv.reader(file)
@@ -15,6 +15,7 @@ for row in data:
 file.close()
 for i in range(0,2):
     feature_set[:,i]=dataMatrix[:,i]
+feature_set[:,2]=feature_set[:,0]*feature_set[:,1]
 labels=dataMatrix[:,2]
 labels=labels.reshape((100,1))
 
@@ -23,8 +24,8 @@ avg=(1/m)*np.sum(feature_set,axis=0)
 rng=np.amax(feature_set,axis=0)-np.amin(feature_set,axis=0)
 feature_set=(feature_set-avg)/rng
 feature_set=np.hstack((np.ones((100,1)),feature_set))
-feature_set=feature_set.reshape((100,3))
-iterNo=2000
+feature_set=feature_set.reshape((100,4))
+iterNo=200
 cost_history=np.ones((iterNo,2))
 
 
@@ -33,7 +34,7 @@ def sigmoid(x):
 def gradient_descent():
     global theta
     random.seed(42)
-    theta = random.random()*np.ones((3, 1))
+    theta = random.random()*np.ones((4, 1))
     learning_rate=0.9
     for i in range(1,iterNo):
         hypothesis = sigmoid(np.dot(feature_set, theta))
@@ -49,7 +50,9 @@ def predict():
     global output
     x1=int(input("Enter the marks for exam 1"))
     x2=int(input("Enter the marks for exam 2"))
-    student=np.array([1,(x1-avg[0])/int(rng[0]),(x2-avg[1])/int(rng[1])])
+    x3=x1*x2
+
+    student=np.array([1,(x1-avg[0])/int(rng[0]),(x2-avg[1])/int(rng[1]),(x3-avg[2])/int(rng[2])])
     output=sigmoid(np.dot(student,theta))
     print("Probability of Passing:",float(output)*100,"%")
     if output>=0.5:
@@ -60,10 +63,10 @@ def plotGraphs():
     x=np.linspace(-0.5,0.5,100)
     x=x.reshape((100,1))
     y=x.reshape((100,1))
-    for j in range(1,3):
+    for j in range(1,4):
          x=np.hstack((y,x))
          j+=1
-    x=x.reshape((100,3))
+    x=x.reshape((100,4))
     plt.title("Hypothesis")
     plt.plot(np.dot(x,theta),sigmoid(np.dot(x,theta)))
     plt.scatter(np.dot(feature_set,theta),labels,marker='X')
@@ -77,13 +80,13 @@ def plotGraphs():
     plt.title("Decision Boundary")
     newFeature_set=np.hstack((feature_set,labels))
     for i in range(0,100):
-        if newFeature_set[i,3]==0:
+        if newFeature_set[i,4]==0:
             plt.scatter(newFeature_set[i,1],newFeature_set[i,2],color='red')
         else:
             plt.scatter(newFeature_set[i,1],newFeature_set[i,2],color='green')
 
     x=np.linspace(-0.5,0.5,100)
-    y=-(theta[0]+theta[1]*x)/theta[2]
+    y=-(theta[0]+theta[1]*x)/(theta[2]+theta[3]*x)
     plt.xlim([-0.5,0.5])
     plt.ylim([-0.5,0.5])
     plt.plot(x,y)
